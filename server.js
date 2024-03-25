a07b6d7c74a18d15408e0d620f453f645df0e2e3
MZuran <83543640+MZuran@users.noreply.github.com>
2024-03-25 11:08:06 -0300

    Añadidas las rutas de productos

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from "express";
+          
 import usersManagers from "./data/fs/UserManager.fs.js";
 import userManagerInstance from "./data/fs/UserManager.fs.js";
+        
+import { fruitManager } from "./data/fs/ProductsManager.fs.js"
+          
 const server = express()
 const port = 8080
 const ready = () => console.log("server ready on port " + port);
@@ -42,7 +46,54 @@ server.get("/api/users", async (req, res) => {
         console.log(error);
         return res.status(error.statusCode).json({
             response: error.message,
+            statusCode: error.statusCode,
             success: false
+        })
+    }
+})
+          
+server.get("/api/products", async (req, res) => {
+    try {
+        const { category } = req.query
+        const productList = await fruitManager.read(category)
+        if (productList.length !== 0) {
+            return res.status(200).json({
+                response: productList,
+                statusCode: res.statusCode
+            })
+        } else {
+            const error = new Error("No matching Products")
+            error.statusCode = 404
+            throw error
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(error.statusCode).json({
+            response: error.message,
+            statusCode: error.statusCode
+        })
+    }
+})
+
+server.get("/api/products/:pid", async (req, res) => {
+    try {
+        const { pid } = req.params
+        const productList = await fruitManager.readOne(pid)
+        if (productList.length !== 0) {
+            return res.status(200).json({
+                response: productList,
+                statusCode: res.statusCode
+            })
+        } else {
+            const error = new Error("No matching Products")
+            error.statusCode = 404
+            throw error
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(error.statusCode).json({
+            response: error.message,
+            statusCode: error.statusCode
         })
     }
 })
@@ -65,9 +116,8 @@ server.get("/api/users/:nid", async (req, res) => {
         console.log(error);
         return res.status(error.statusCode).json({
             response: error.message,
-            success: false
+            statusCode: error.statusCode
         })
     }
 })
-
-
+            
