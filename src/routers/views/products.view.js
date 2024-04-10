ebde6f111358aad338d48c4de9757dbfde121e63
MariaNavarro90 <navarromariavictoria8@gmail.com>
2024-04-09 23:19:17 -0300

    added fruits views

--- src/routers/views/products.view.js
+++ src/routers/views/products.view.js
@@ -4,11 +4,24 @@ import { fruitManager } from "../../data/fs/ProductsManager.fs.js";
 export const productsViewRouter = Router();
 
 productsViewRouter.get("/", productsView);
+productsViewRouter.get("/:nid", productsViewOne);
+
 
 async function productsView(req, res, next) {
     try {
-        return res.render("products", { title: "TEST" });
+        const fruits = await fruitManager.read();
+        return res.render("products", { title: "PRODUCTS", products: fruits });
     } catch (error) {
         next(error)
     }
 }
+
+ async function productsViewOne(req, res, next) {
+    try {
+      const { nid } = req.params;
+      const one = await fruitManager.readOne(nid);
+      return res.render("details", { title: "DETAILS", fruit: one });
+    } catch (error) {
+      return next(error);
+    }
+  }
