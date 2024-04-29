aee96f57f0cbe8d61a8a283f679093a24f3d260a
MariaNavarro90 <navarromariavictoria8@gmail.com>
2024-04-29 20:48:30 -0300

    created cart model and manager with mongo

--- src/routers/api/index.api.js
+++ src/routers/api/index.api.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import cartRouter from "./carts.routers.api.js";
 import usersRouter from "./users.api.js";
 import productsRouter from "./products.api.js";
 const apiRouter = Router();
 
 apiRouter.use("/users", usersRouter);
 apiRouter.use("/products", productsRouter);
+apiRouter.use("/carts", cartRouter);
 
 export default apiRouter;
