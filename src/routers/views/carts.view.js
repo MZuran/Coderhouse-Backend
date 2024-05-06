f4747135d33998f5836b5f0addd7d83e615d31ed
MariaNavarro90 <navarromariavictoria8@gmail.com>
2024-05-06 16:11:35 -0300

    add to cart icon and login

---
+++ src/routers/views/carts.view.js
@@ -0,0 +1,39 @@
+import { Router } from "express"
+import productManagerMongo from "../../data/mongo/managers/cartsManager.mongo.js";
+
+const selectedManager = productManagerMongo
+
+export const productsViewRouter = Router();
+
+productsViewRouter.get("/", productsView);
+productsViewRouter.get("/real", productsViewReal);
+productsViewRouter.get("/:nid", productsViewOne);
+
+
+
+async function productsView(req, res, next) {
+    try {
+        const fruits = await selectedManager.read();
+        return res.render("products", { title: "PRODUCTS", products: fruits });
+    } catch (error) {
+        next(error)
+    }
+}
+
+ async function productsViewOne(req, res, next) {
+    try {
+      const { nid } = req.params;
+      const one = await selectedManager.readOne(nid);
+      return res.render("details", { title: "DETAILS", fruit: one });
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  async function productsViewReal(req, res, next) {
+    try {
+      return res.render("real", { title: "REAL" });
+    } catch (error) {
+      return next(error);
+    }
+  }
