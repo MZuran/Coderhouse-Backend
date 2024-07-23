e9c5376f9918ca8086f9a359528ffd2d486d469f
MariaNavarro90 <navarromariavictoria8@gmail.com>
2024-07-22 22:08:42 -0300

    Ruta POST y PUT definidas

--- src/routers/api/sessions.api.js
+++ src/routers/api/sessions.api.js
@@ -2,13 +2,15 @@ import { Router } from "express";
 import passport from "../../middlewares/passport.mid.js";
 import CustomRouter from "../customRouter.js";
 import passportCb from "../../middlewares/passportCb.mid.js";
-import { readSessions, registerSession, loginSession, checkOnlineStatus, signOutSession, googleCallback } from "../../controllers/sessions.controller.js"
+import { readSessions, registerSession, loginSession, checkOnlineStatus, signOutSession, googleCallback, sendPasswordResetEmail, updatePassword } from "../../controllers/sessions.controller.js"
 
 class sessionsRouterClass extends CustomRouter {
   init() {
     this.read("/", readSessions);
     this.create("/register", ["PUBLIC"], passport.authenticate("register", { session: false }), registerSession);
     this.create("/login", ["PUBLIC"], passportCb("login"), loginSession);
+    this.create("/password", ["PUBLIC"], sendPasswordResetEmail);//revisar que este ok
+    this.update("/password", ["USER", "ADMIN"], updatePassword); //y que esto tambien
     this.read("/online", ["USER", "ADMIN"], checkOnlineStatus);
     this.create("/signout", ["USER", "ADMIN"], signOutSession);
     this.read("/google", ["PUBLIC"], passport.authenticate("google", { scope: ["email", "profile"] }));
@@ -16,5 +18,7 @@ class sessionsRouterClass extends CustomRouter {
   }
 }
 const sessionsRouter = new sessionsRouterClass();
+
+
 
 export default sessionsRouter.getRouter();
