537cec865bf96e5ae1b785f07e970fe41ceb69a4
Maria Victoria Navarro <86987622+MariaNavarro90@users.noreply.github.com>
2024-08-31 18:23:26 -0300

    Documentacion (#30)
    * chore: Update npm dependencies and Swagger documentation
    
    Update the npm dependencies "stripe" from version 16.8.0 to 16.9.0 in the package.json and package-lock.json files. Also, update the Swagger documentation in swagger.util.js to include the new API files for ticket and payment. This commit improves the project's dependencies and keeps the documentation up to date.
    
    * feat: Add carousel with welcome messages to landing page
    
    This commit adds a carousel to the welcome.handlebars view on the landing page. The carousel displays three images with corresponding welcome messages. Each image is set to cover the entire viewport height. The carousel is interactive and can be navigated using the previous and next buttons. This enhancement improves the visual appeal and user experience of the landing page.
    
    Co-authored-by: MariaNavarro90 <navarromariavictoria8@gmail.com>

--- src/utils/swagger.util.js
+++ src/utils/swagger.util.js
@@ -13,7 +13,7 @@ const options = {
             description: "Documentation of Green Groceries API"
         },
     },
-    apis: [path.join(__dirname, "..\\docs\\users.doc.yaml"), path.join(__dirname, "..\\docs\\product.doc.yaml"), path.join(__dirname, "..\\docs\\sessions.doc.yaml"), path.join(__dirname, "..\\docs\\carts.doc.yaml"), path.join(__dirname, "..\\docs\\tickets.doc.yaml"), path.join(__dirname, "..\\docs\\cookies.doc.yaml"), path.join(__dirname, "..\\docs\\payment.doc.yaml")],
+    apis: [path.join(__dirname, "..\\docs\\users.doc.yaml"), path.join(__dirname, "..\\docs\\product.doc.yaml"), path.join(__dirname, "..\\docs\\sessions.doc.yaml"), path.join(__dirname, "..\\docs\\carts.doc.yaml"), path.join(__dirname, "..\\docs\\tickets.doc.yaml"), path.join(__dirname, "..\\docs\\cookies.doc.yaml"), path.join(__dirname, "..\\docs\\payment.doc.yaml")], 
     debug: {
         dirName: __dirname
     }
